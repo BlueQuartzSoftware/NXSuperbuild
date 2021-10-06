55554309e5a306583911ab5b19a24854c91c5b1e
Michael Jackson <mike.jackson@bluequartz.net>
2021-10-06 16:34:18 -0400

    VTK: bump to version 9.1.0.rc1
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- projects/VTK.cmake
+++ projects/VTK.cmake
@@ -15,9 +15,9 @@ if("${BUILD_VTK}" STREQUAL "OFF")
 endif()
 
 set(extProjectName "VTK")
-set(VTK_GIT_TAG "v9.0.3")
-set(VTK_VERSION "9.0.3")
-set(VTK_VERSION_SHORT "9.0")
+set(VTK_GIT_TAG "v9.1.0.rc1")
+set(VTK_VERSION "9.1.0")
+set(VTK_VERSION_SHORT "9.1")
 
 message(STATUS "Building: ${extProjectName} ${VTK_VERSION}: -DBUILD_VTK=${BUILD_VTK}" )
 
